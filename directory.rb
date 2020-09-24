35cf4d023a686e8e801034d95a4b46eba12c4a1f
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-09-24 15:13:08 +0100

    Saved the name & cohort inputs to a file

--- directory.rb
+++ directory.rb
@@ -1,32 +1,5 @@
 @students = [] # an empty array accessible to all methods
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit"
-end
-def show_students
-  print_header
-  print_student_list
-  print_footer
-end
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-  end
-end
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -37,17 +10,67 @@ def input_students
     name = gets.chomp
   end
 end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
+
 def print_student_list
   @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+
 def print_footer
   puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of save_students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
