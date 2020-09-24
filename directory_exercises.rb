f3d8074350fcf2bf242db359778713e0b2e3f52d
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-09-24 09:24:37 +0100

    Exercise 8: Students now printed grouped by cohorts

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -1,7 +1,9 @@
 def input_students
   students = []
-  cohorts = [:january, :february, :march, :april, :may, :june, :july, :august,
+  total_cohorts = [:january, :february, :march, :april, :may, :june, :july, :august,
   :september, :october, :november, :december]
+  current_cohorts = Hash.new
+
   while true
     puts "Please enter student name"
     name = gets.chomp.capitalize
@@ -11,32 +13,30 @@ def input_students
     while true
       cohort = gets.chomp.downcase.to_sym
       cohort = :november if cohort.empty?
-      if cohorts.include?(cohort)
+      if total_cohorts.include?(cohort)
         cohort
         break
       else
         puts "Error, please re-enter cohort"
       end
     end
-    students << {name: name, cohort: cohort}
 
-    puts "Now we have #{students.count} students"
+    current_cohorts.key?(cohort) ? current_cohorts[cohort] << name : current_cohorts[cohort] = [name]
+    students << {name: name, cohort: cohort}
   end
-  students
+  current_cohorts
 end
 def print_header
   puts "The students of Villains Academy".center(100)
   puts "-------------".center(100)
 end
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]}, (#{student[:cohort]} cohort)".center(100)
-  end
+def print(current_cohorts)
+  current_cohorts.map{ |key, value| puts "#{key}: #{value}".center(100) }
 end
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students".center(100)
+def print_footer(current_cohorts)
+  puts "Overall, we have #{current_cohorts.count} great cohorts".center(100)
 end
-students = input_students
+current_cohorts = input_students
 print_header
-print(students)
-print_footer(students)
+print(current_cohorts)
+print_footer(current_cohorts)
